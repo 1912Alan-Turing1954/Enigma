2e6ab24a6ae6298b6539226120ea876573eb905f
Alan M. Turing <1912AlanTuring1954@gmail.com>
2022-08-21 09:51:47 -0700

    Initial Commit.

---
+++ enigma-machine.js
@@ -0,0 +1,47 @@
+import { spindle1, spindle2, spindle3 } from "./spindles"
+
+const wirePairs = {
+    a: 1,
+    b: 2,
+    c: 3,
+    d: 4, 
+    e: 5, 
+    f: 6,
+    g: 7,
+    h: 8,
+    i: 9,
+    j: 10,
+    k: 11,
+    l: 12,
+    m: 13, 
+    n: 14,
+    o: 15,
+    p: 16,
+    q: 17,
+    r: 18,
+    s: 19,
+    t: 20,
+    u: 21,
+    v: 22,
+    w: 23,
+    x: 24,
+    y: 25,
+    z: 26,
+    ' ': '-',
+};
+
+function enigmaMachine (
+    message,
+    configuration,
+    spindle1Offset,
+    spindle2Offset,
+    spindle3Offset,
+    rotatePoint1,
+    rotatePoint2,
+) {
+    let messageArr = message.split('').map(el => wirePairs[el]);
+    
+    return codedMessage.join('');
+}
+
+console.log(enigmaMachine('hello'));
