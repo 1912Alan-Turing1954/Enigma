0c33a003b7fb5cf297720d104ea19eec0ef829a1
Alan M. Turing <1912AlanTuring1954@gmail.com>
2022-08-21 14:29:02 -0700

    Alphabet Commit.

--- enigma-machine.js
+++ enigma-machine.js
@@ -29,6 +29,35 @@ const wirePairs = {
     z: 25,
 };
 
+const alphabet = {
+    0: 'a',
+    1: 'b',
+    2: 'c',                        
+    3: 'd', 
+    4: 'e',
+    5: 'f',
+    6: 'g',
+    7: 'h',
+    8: 'i',
+    9: 'j',
+    10: 'k',
+    11: 'l', 
+    12: 'm', 
+    13: 'n',
+    14: 'o',
+    15: 'p',
+    16: 'q',
+    17: 'r',
+    18: 's',
+    19: 't',
+    20: 'u',
+    21: 'v',
+    22: 'w',
+    23: 'x',
+    24: 'y',
+    25: 'z', 
+}
+
 const spindleA = {
     0: 22,
     1: 9,
@@ -172,7 +201,7 @@ function enigmaMachine (
         return answer;
     });
         
-    return codedMessage;
+    return codedMessage.map(el => alphabet[el]).join('');
 }
 
 console.log(enigmaMachine('', config1, 22, 24, 0, 25, 25));
